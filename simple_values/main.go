4d164fd1c598b15cecf0b2ebd41773b3568e6599
Rehan Halai <halairehan01@gmail.com>
2026-08-10 20:18:58 +0530

    first commit

---
+++ simple_values/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1)
+	fmt.Println(true)
+	fmt.Println("string")
+	fmt.Println(108*6 + (102 / 2))
+}
---
+++ variables/variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "Jogh Senna"
+	var name = "John Senna"
+
+	var isEligible bool = true
+
+	// short hand
+	subject := "GoLang"
+
+	fmt.Println(name)
+	fmt.Println(isEligible)
+	fmt.Println(subject)
+
+}
