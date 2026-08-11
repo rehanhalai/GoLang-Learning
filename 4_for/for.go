a6bfd3b45559b0be73c4d51abc23d65cf465b222
Rehan Halai <halairehan01@gmail.com>
2026-08-11 11:25:31 +0530

    feat: add examples of for loop syntax and iteration in Go

---
+++ 4_for/for.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// for is the only looping statement in the GoLang
+
+func main() {
+	// i := 1
+	// for i <= 5 {
+	// fmt.Println(i)
+	// i++
+	// }
+
+	// infinite loop
+	// for {
+	// println("infinite")
+	// }
+
+	// for i := 0; i < 3; i++ {
+	// fmt.Println(i)
+	// }
+
+	//range
+
+	for i := range 3 {
+		fmt.Println(i)
+	}
+
+}
